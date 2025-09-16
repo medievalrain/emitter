f5d4af39aa21bc2fa3656546a81762a493915c7a
Vladislav Borzhemski <dev@vlad.ph>
2025-09-16 13:44:54 +0300

    Second iteration

--- src/emitter.ts
+++ src/emitter.ts
@@ -1,25 +1,52 @@
 export type EventMap = Record<string, (...args: any[]) => void>;
 
+export type CallbackOptions = {
+	once?: boolean;
+	signal?: AbortSignal;
+};
+
 export const createEmitter = <Events extends EventMap>() => {
 	type EventName = keyof Events;
-	const callbackMap = new Map<EventName, Set<Events[EventName]>>();
+	const callbackMap = new Map<EventName, Map<Events[EventName], boolean | undefined>>();
+
+	const off = <EM extends EventName>(event: EM, callback: Events[EM]) => {
+		const callbacks = callbackMap.get(event);
+		if (!callbacks) {
+			return;
+		}
+		callbacks.delete(callback);
+	};
 
 	const emit = <EM extends EventName>(event: EM, ...args: Parameters<Events[EM]>) => {
 		const callbacks = callbackMap.get(event);
 		if (!callbacks?.size) {
 			return;
 		}
-		Array.from(callbacks?.values()).forEach((callback) => callback(args));
+		Array.from(callbacks?.entries()).forEach(([callback, once]) => {
+			callback(...args);
+			if (once) {
+				off(event, callback);
+			}
+		});
 	};
 
-	const on = <EM extends EventName>(event: EM, callback: Events[EM]) => {
+	const on = <EM extends EventName>(event: EM, callback: Events[EM], options?: CallbackOptions) => {
 		let callbacks = callbackMap.get(event);
 		if (!callbacks?.size) {
-			callbacks = new Set<Events[EM]>();
+			callbacks = new Map<Events[EM], boolean | undefined>();
 			callbackMap.set(event, callbacks);
 		}
-		callbacks.add(callback);
+		if (options?.signal) {
+			options.signal.addEventListener(
+				"abort",
+				() => {
+					off(event, callback);
+				},
+				{ once: true }
+			);
+		}
+		callbacks.set(callback, options?.once);
 	};
 
-	return { emit, on };
+	return { emit, on, off };
 };
