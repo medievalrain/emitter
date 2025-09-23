3a7e4625e9eff2102fb98bf40083974a51ede4a1
Vladislav Borzhemski <dev@vlad.ph>
2025-09-23 21:23:14 +0300

    Add formatting

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vitest/config";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
 });
